cbd5cc65d907b64244ed0d6369751e950bdab338
surayya-lab <76586836+surayya-lab@users.noreply.github.com>
2022-12-04 15:36:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let ground;
+let lander;
+var lander_img;
+var bg_img;
+
+
+var vx = 0;
+var g = 0.05;
+var vy = 0;
+
+function preload()
+{
+}
+
+function setup() {
+  createCanvas(1000,700);
+ 
+}
+
+function draw() 
+{
+  background(51);
+ 
+}
+
+
